251a68689c0f6f3d26a58d44c3bdea82a58d9ff9
TestMachine <test@deviceintegration-macmini.guest.corp.microsoft.com>
2016-07-06 17:02:08 -0700

    Initial Commit

---
+++ testNative/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testNative
+//
+//  Created by TestMachine on 7/6/16.
+//  Copyright © 2016 TestMachine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testNative/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testNative
+//
+//  Created by TestMachine on 7/6/16.
+//  Copyright © 2016 TestMachine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
